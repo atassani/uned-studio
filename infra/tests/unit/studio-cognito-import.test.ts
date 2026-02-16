eea17a988f4f6f0548ab7c52d28d7a94faa87582
Toni Tassani <toni.tassani@ocado.com>
2026-02-16 18:43:53 +0100

    test: cover existing cognito client import

--- infra/tests/unit/studio-cognito-import.test.ts
+++ infra/tests/unit/studio-cognito-import.test.ts
@@ -17,6 +17,7 @@ describe('StudioCognito with existing user pool', () => {
     process.env.EXISTING_COGNITO_USER_POOL_ID = 'eu-west-2_lGf1JmMyv';
     process.env.NEXT_PUBLIC_COGNITO_DOMAIN =
       'https://humblyproud-studio.auth.eu-west-2.amazoncognito.com';
+    process.env.EXISTING_COGNITO_USER_POOL_CLIENT_ID = 'existing-client-id';
     process.env.GOOGLE_OAUTH_CLIENT_ID = 'test-client-id';
     process.env.GOOGLE_OAUTH_CLIENT_SECRET = 'test-client-secret';
 
@@ -28,5 +29,6 @@ describe('StudioCognito with existing user pool', () => {
     const template = Template.fromStack(stack);
     template.resourceCountIs('AWS::Cognito::UserPool', 0);
     template.resourceCountIs('AWS::Cognito::UserPoolDomain', 0);
+    template.resourceCountIs('AWS::Cognito::UserPoolClient', 0);
   });
 });
