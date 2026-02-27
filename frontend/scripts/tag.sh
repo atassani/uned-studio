b593956355e8a151dffc308fc0c1c91ed6c360c3
Toni Tassani <toni.tassani@ocado.com>
2026-02-27 17:45:06 +0100

    docs: update tag.sh script with documentation

--- frontend/scripts/tag.sh
+++ frontend/scripts/tag.sh
@@ -1,3 +1,15 @@
+cd /Users/toni.tassani/code/humblyproud-multiproject/learning-studio
+
+npm version patch --workspace=frontend --no-git-tag-version
+git add frontend/package.json package-lock.json
+git commit -m "chore(release): frontend v$(node -p \"require('./frontend/package.json').version\")"
+git tag v$(node -p "require('./frontend/package.json').version")
+git push origin HEAD --tags
+
+## From project root (DOES NOT WORK)
+## This will update package.json version, create a git tag, and push the tag to the remote repository.
+## major | minor | patch
+npm version patch --workspace=frontend
 
 
 git tag -a v1.4.X -m "Release v1.4.X"
